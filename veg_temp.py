0673d6f1acefd3523e6f740cfc1b91c45e4aa9e1
Morgan Ernest <morgan@weecology.org>
2012-11-15 13:04:46 -0700

    Checks site names, prints ok if meets conditions

--- veg_temp.py
+++ veg_temp.py
@@ -6,13 +6,33 @@ def get_badcodes_indata(datafiles, specieslist):
     unique_species_data = set(data['species'])
     unique_species_list = set(species_list['Sp_Code'])
     bad_codes = unique_species_data - unique_species_list
-    return bad_codes
+    if bad_codes:
+        return bad_codes
+    else:
+        return "Species codes: ok"
 
+def find_sitename_typos(data):
+    """Checks if Site name is consistent in file"""
+    Extra_sitename_versions = set(data['Site'])
+    if len(Extra_sitename_versions) > 1:
+        return Extra_sitename_versions
+    else:
+        return "Site Name: ok"
+
+"""main code"""    
 filename = "RiparianNorth_transects_2012.csv"
 data = np.genfromtxt(filename, dtype=None, names = True, delimiter=",")
 
 filename2 = "SpList.csv"
 species_list = np.genfromtxt(filename2, dtype=None, names = True, delimiter=",")
 
-"""main code"""
-bad_speciescodes=get_badcodes_indata(data, species_list)
+print get_badcodes_indata(data, species_list)
+print find_sitename_typos(data)
+
+#Test area below
+
+
+
+    
+
+
